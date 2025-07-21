86d24b1c6ea4eb545c052e0b12d258d55568a111
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-21 05:22:44 -0600

    create mongodb folder and connected to the server

---
+++ config/mongodb.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  await mongoose.connect(`${process.env.MONGODB_URL}/MERN-AUTH`);
+};
+export default connectDB;
