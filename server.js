ac0c51ff04012abf423131e1473b986e84a3d66c
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-21 03:58:31 -0600

    updated

--- server.js
+++ server.js
@@ -3,3 +3,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.get("/", (req, res) => res.send("API is working"));
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
