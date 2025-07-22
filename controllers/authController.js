2946b0da240ccf76e3c42a824a857516e6616da1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-22 00:39:13 -0600

    completed the login

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,6 +32,7 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+    return res.json({ success: true });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
@@ -58,6 +59,17 @@ export const login = async (req, res) => {
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid password" });
     }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({ success: true });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
