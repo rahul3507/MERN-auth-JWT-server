2f720fea6c2a42426f2d0bc71eb32af4778e4d98
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-21 21:47:39 -0600

    export the userModel

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,11 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   verifyOtp: { type: String, default: "" },
-  verifyOtpExpireAt: { type: Number, default: "" },
+  verifyOtpExpireAt: { type: Number, default: 0 },
   isAccountVerified: { type: Boolean, default: false },
+  resetOtp: { type: String, default: "" },
+  resetOtpExpireAt: { type: Number, default: 0 },
 });
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
