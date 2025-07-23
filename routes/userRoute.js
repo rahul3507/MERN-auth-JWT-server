24f8eea97e738912c296ba590e15b17821520975
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-23 05:26:21 -0600

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,11 @@
 /** @format */
 
 import express from "express";
+import userAuth from "../middleware/userAuth.js";
+import { getUserData } from "../controllers/userController.js";
 
 const userRouter = express.Router();
+
+userRouter.get("/data", userAuth, getUserData);
+
+export default userRouter;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,5 +20,6 @@ app.use(cors({ credentials: true }));
 //API Endpoints
 app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
