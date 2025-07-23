b0857a96f6e536071d52712682634792918217ad
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-22 22:34:23 -0600

    complete tje sendverifyotp perfectly

--- controllers/authController.js
+++ controllers/authController.js
@@ -107,7 +107,7 @@ export const logout = async (req, res) => {
 export const sendVerifyOtp = async (req, res) => {
   try {
     const { userId } = req.body;
-    const user = await userModel.findById();
+    const user = await userModel.findById(userId);
     if (user.isAccountVerified) {
       return res.json({
         success: false,
@@ -115,8 +115,17 @@ export const sendVerifyOtp = async (req, res) => {
       });
     }
     const otp = String(Math.floor(100000 + Math.random() * 900000));
-    use.verifyOtp = otp;
-    user.verfyOtp;
+    user.verifyOtp = otp;
+    user.verfyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+    await user.save();
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: "Account Verification OTP",
+      text: `Your OTP is ${otp}. Verify your account using this OTP`,
+    };
+    await transporter.sendMail(mailOptions);
+    res.json({ success: true, message: "Verification OTP Sent on Email" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
