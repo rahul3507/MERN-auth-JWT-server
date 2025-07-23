14a54a24f0b1410f6b7b88c11e33cd2f469bebe8
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-22 22:50:29 -0600

    created userAuth

---
+++ middleware/userAuth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const userAuth =async(req,res,next){
+    const {token}=req.cookies;
+
+    if(!token){
+        return res.json({success:false, message:'Not Authorized. Login Again'})
+    }
+    
+
+    try{
+
+      const tokenDecode=  jwt.verify(token,process.env.JWT_SECRET);
+      if(tokenDecode.id){
+        req.body
+      }
+    }catch(error){
+        res.json({success:false, message:error.message})
+    }
+}
