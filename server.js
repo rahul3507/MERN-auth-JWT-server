29eb2578871548366dcd22a4203162fc249c95a1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-24 03:54:03 -0600

    set the origins/frontend url into the cors

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 
+const allowedOrigins = ["http://localhost:5173"];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-//API Endpoints
+//API Endpointsset
 app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
